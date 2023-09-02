2df54779c9d700665a55db144ec98c7a8a151c66
MDixey17 <matthew_dixey@yahoo.com>
2023-09-01 19:52:44 -0500

    working websocket service

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { WebsocketService } from "./services/websocketService";
 
 const App = () => {
+  useEffect(() => {
+    WebsocketService.init(49322, true);
+    WebsocketService.subscribe("game", "update_state", (data: any) => {
+      console.log(data);
+    });
+  }, []);
+
   return <div>Overlay components here</div>;
 };
 
