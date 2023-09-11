236faefa40e9839ba2f052427171ee26d4b39dec
MDixey17 <matthew_dixey@yahoo.com>
2023-09-10 21:38:24 -0500

    goal replay, start of postgame

--- src/components/GoalReplay/GoalReplay.tsx
+++ src/components/GoalReplay/GoalReplay.tsx
@@ -1,6 +1,27 @@
-/**
- * TODO: This component should render when a goal replay is occurring.
- */
-export const GoalReplay = () => {
-  return <></>;
+import {
+  GoalReplayPasser,
+  GoalReplayScorer,
+  GoalReplaySpeed,
+  GoalReplayWrapper,
+} from "./GoalReplay.style";
+
+interface GoalReplayProps {
+  scorer: string;
+  passer: string | undefined;
+  speed: number;
+  teamPrimary: string;
+  teamSecondary: string;
+}
+
+export const GoalReplay = (props: GoalReplayProps) => {
+  const { scorer, passer, speed, teamPrimary, teamSecondary } = props;
+  return (
+    <GoalReplayWrapper $primary={teamPrimary} $secondary={teamSecondary}>
+      {passer !== undefined && (
+        <GoalReplayPasser>{`Assist: ${passer}`}</GoalReplayPasser>
+      )}
+      <GoalReplayScorer>Goal: {scorer}</GoalReplayScorer>
+      <GoalReplaySpeed>Speed: {speed} KPH</GoalReplaySpeed>
+    </GoalReplayWrapper>
+  );
 };
